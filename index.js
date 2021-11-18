dedc94d60f7a401ba6e46997a850210063acddb9
Michael Newman <therealmichaelnewman@gmail.com>
2021-11-18 17:29:17 -0500

    modified index.js and gameboard.html

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("working?");
+const playerClicks = document.querySelectorAll(".squares-click");
+
+for (i = 0; i < playerClicks.length; i++) {
+  playerClicks[i].addEventListener("click", (e) => {
+    console.log(e.target.getAttribute("id"));
+    console.log(document.getElementById("squ-7"));
+
+    if (e.target.getAttribute("id") === "squ-0") {
+      if (
+        document.getElementById("squ-7").getAttribute("style") ===
+        "background-color: white"
+      ) {
+        //console.log(document.getElementById("squ-7").style.color);
+        document.getElementById("squ-7").style = "background-color: black";
+      }
+    }
+  });
+}
+
+// const gameMove = () => {
+//     if () {
+
+//     }
+
+// }
