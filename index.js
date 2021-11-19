1b1f196f576a9f01ad053ea6ffeac770676e898a
Michael Newman <therealmichaelnewman@gmail.com>
2021-11-19 11:01:08 -0500

    updated playerClicks function in index.js

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
+const colOne = document.querySelectorAll(".col-1");
+
+////////////////
+
 const playerClicks = document.querySelectorAll(".squares-click");
 
 for (i = 0; i < playerClicks.length; i++) {
   playerClicks[i].addEventListener("click", (e) => {
-    console.log(e.target.getAttribute("id"));
-    console.log(document.getElementById("squ-7"));
-
-    if (e.target.getAttribute("id") === "squ-0") {
-      if (
-        document.getElementById("squ-7").getAttribute("style") ===
-        "background-color: white"
-      ) {
-        //console.log(document.getElementById("squ-7").style.color);
-        document.getElementById("squ-7").style = "background-color: black";
-      }
+    if (
+      document.getElementById("squ-42").getAttribute("style") ===
+      "background-color: white"
+    ) {
+      document.getElementById("squ-42").style = "background-color: black";
+    } else if (
+      document.getElementById("squ-35").getAttribute("style") ===
+      "background-color: white"
+    ) {
+      document.getElementById("squ-35").style = "background-color: black";
+    } else if (
+      document.getElementById("squ-28").getAttribute("style") ===
+      "background-color: white"
+    ) {
+      document.getElementById("squ-28").style = "background-color: black";
+    } else if (
+      document.getElementById("squ-21").getAttribute("style") ===
+      "background-color: white"
+    ) {
+      document.getElementById("squ-21").style = "background-color: black";
+    } else if (
+      document.getElementById("squ-14").getAttribute("style") ===
+      "background-color: white"
+    ) {
+      document.getElementById("squ-14").style = "background-color: black";
+    } else if (
+      document.getElementById("squ-7").getAttribute("style") ===
+      "background-color: white"
+    ) {
+      document.getElementById("squ-7").style = "background-color: black";
     }
   });
 }
-
-// const gameMove = () => {
-//     if () {
-
-//     }
-
-// }
