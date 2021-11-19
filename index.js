8a33803e6c1bff5f62d81801a2e7ff2637a3d9a1
Michael Newman <therealmichaelnewman@gmail.com>
2021-11-19 16:46:29 -0500

    created and updated checkWins function

--- index.js
+++ index.js
@@ -7,56 +7,106 @@ const colFour = document.querySelector("#squ-4");
 const colFive = document.querySelector("#squ-5");
 const colSix = document.querySelector("#squ-6");
 
+const squares = document.querySelectorAll(".squares");
+
 let clicks = 0;
 
-let color = "red";
+let color = "hotpink";
 
 const currentColor = () => {
-  clicks % 2 === 0 ? (color = "red") : (color = "black");
+  clicks % 2 === 0 ? (color = "hotpink") : (color = "black");
 };
 
-console.log(clicks);
-console.log(color);
+///////////// winning combinations
 
-//playermoves events
+const gameWins = [
+  [35, 36, 37, 38],
+  [36, 37, 38, 39],
+  [37, 38, 39, 40],
+  [38, 39, 40, 41],
+  [28, 29, 30, 31],
+  [29, 30, 31, 32],
+  [30, 31, 32, 33],
+  [31, 32, 33, 34],
+  [21, 22, 23, 24],
+  [22, 23, 24, 25],
+  [23, 24, 25, 26],
+  [24, 25, 26, 27],
+  [14, 15, 16, 17],
+  [15, 16, 17, 18],
+  [16, 17, 18, 19],
+  [17, 18, 19, 20],
+  [7, 8, 9, 10],
+  [8, 9, 10, 11],
+  [9, 10, 11, 12],
+  [10, 11, 12, 13],
+  [0, 1, 2, 3],
+  [1, 2, 3, 4],
+  [2, 3, 4, 5],
+  [3, 4, 5, 6],
+  [0, 7, 14, 21],
+  [7, 14, 21, 28],
+  [14, 21, 28, 35],
+  [1, 8, 15, 22],
+  [8, 15, 22, 29],
+  [15, 22, 29, 36],
+];
+
+const checkWins = () => {
+  for (i = 0; i < gameWins.length; i++) {
+    let squ1 = squares[gameWins[i][0]];
+    let squ2 = squares[gameWins[i][1]];
+    let squ3 = squares[gameWins[i][2]];
+    let squ4 = squares[gameWins[i][3]];
+
+    if (
+      squ1.style.backgroundColor === `${color}` &&
+      squ2.style.backgroundColor === `${color}` &&
+      squ3.style.backgroundColor === `${color}` &&
+      squ4.style.backgroundColor === `${color}`
+    ) {
+      console.log(`${color} wins!`);
+    }
+  }
+};
+
+////////////// playermoves events
 
 colZero.addEventListener("click", () => {
   if (
     document.getElementById("squ-42").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-42").style = "background-color: black";
+    document.getElementById("squ-42").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-35").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-35").style = "background-color: black";
+    document.getElementById("squ-35").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-28").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-28").style = "background-color: black";
+    document.getElementById("squ-28").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-21").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-21").style = "background-color: black";
+    document.getElementById("squ-21").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-14").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-14").style = "background-color: black";
+    document.getElementById("squ-14").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-7").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-7").style = "background-color: black";
+    document.getElementById("squ-7").style = `background-color: ${color}`;
   }
-
+  checkWins();
   clicks++;
   currentColor();
-  console.log(clicks);
-  console.log(color);
 });
 
 colOne.addEventListener("click", () => {
@@ -64,33 +114,36 @@ colOne.addEventListener("click", () => {
     document.getElementById("squ-43").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-43").style = "background-color: black";
+    document.getElementById("squ-43").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-36").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-36").style = "background-color: black";
+    document.getElementById("squ-36").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-29").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-29").style = "background-color: black";
+    document.getElementById("squ-29").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-22").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-22").style = "background-color: black";
+    document.getElementById("squ-22").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-15").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-15").style = "background-color: black";
+    document.getElementById("squ-15").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-8").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-8").style = "background-color: black";
+    document.getElementById("squ-8").style = `background-color: ${color}`;
   }
+  checkWins();
+  clicks++;
+  currentColor();
 });
 
 colTwo.addEventListener("click", () => {
@@ -98,33 +151,36 @@ colTwo.addEventListener("click", () => {
     document.getElementById("squ-44").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-44").style = "background-color: black";
+    document.getElementById("squ-44").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-37").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-37").style = "background-color: black";
+    document.getElementById("squ-37").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-30").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-30").style = "background-color: black";
+    document.getElementById("squ-30").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-23").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-23").style = "background-color: black";
+    document.getElementById("squ-23").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-16").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-16").style = "background-color: black";
+    document.getElementById("squ-16").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-9").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-9").style = "background-color: black";
+    document.getElementById("squ-9").style = `background-color: ${color}`;
   }
+  checkWins();
+  clicks++;
+  currentColor();
 });
 
 colThree.addEventListener("click", () => {
@@ -132,33 +188,36 @@ colThree.addEventListener("click", () => {
     document.getElementById("squ-45").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-45").style = "background-color: black";
+    document.getElementById("squ-45").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-38").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-38").style = "background-color: black";
+    document.getElementById("squ-38").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-31").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-31").style = "background-color: black";
+    document.getElementById("squ-31").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-24").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-24").style = "background-color: black";
+    document.getElementById("squ-24").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-17").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-17").style = "background-color: black";
+    document.getElementById("squ-17").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-10").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-10").style = "background-color: black";
+    document.getElementById("squ-10").style = `background-color: ${color}`;
   }
+  checkWins();
+  clicks++;
+  currentColor();
 });
 
 colFour.addEventListener("click", () => {
@@ -166,33 +225,36 @@ colFour.addEventListener("click", () => {
     document.getElementById("squ-46").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-46").style = "background-color: black";
+    document.getElementById("squ-46").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-39").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-39").style = "background-color: black";
+    document.getElementById("squ-39").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-32").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-32").style = "background-color: black";
+    document.getElementById("squ-32").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-25").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-25").style = "background-color: black";
+    document.getElementById("squ-25").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-18").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-18").style = "background-color: black";
+    document.getElementById("squ-18").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-11").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-11").style = "background-color: black";
+    document.getElementById("squ-11").style = `background-color: ${color}`;
   }
+  checkWins();
+  clicks++;
+  currentColor();
 });
 
 colFive.addEventListener("click", () => {
@@ -200,33 +262,36 @@ colFive.addEventListener("click", () => {
     document.getElementById("squ-47").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-47").style = "background-color: black";
+    document.getElementById("squ-47").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-40").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-40").style = "background-color: black";
+    document.getElementById("squ-40").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-33").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-33").style = "background-color: black";
+    document.getElementById("squ-33").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-26").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-26").style = "background-color: black";
+    document.getElementById("squ-26").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-19").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-19").style = "background-color: black";
+    document.getElementById("squ-19").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-12").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-12").style = "background-color: black";
+    document.getElementById("squ-12").style = `background-color: ${color}`;
   }
+  checkWins();
+  clicks++;
+  currentColor();
 });
 
 colSix.addEventListener("click", () => {
@@ -234,31 +299,34 @@ colSix.addEventListener("click", () => {
     document.getElementById("squ-48").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-48").style = "background-color: black";
+    document.getElementById("squ-48").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-41").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-41").style = "background-color: black";
+    document.getElementById("squ-41").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-34").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-34").style = "background-color: black";
+    document.getElementById("squ-34").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-27").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-27").style = "background-color: black";
+    document.getElementById("squ-27").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-20").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-20").style = "background-color: black";
+    document.getElementById("squ-20").style = `background-color: ${color}`;
   } else if (
     document.getElementById("squ-13").getAttribute("style") ===
     "background-color: white"
   ) {
-    document.getElementById("squ-13").style = "background-color: black";
+    document.getElementById("squ-13").style = `background-color: ${color}`;
   }
+  checkWins();
+  clicks++;
+  currentColor();
 });
